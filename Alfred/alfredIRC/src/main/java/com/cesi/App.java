b715d55a802425c612d122922367a14caf8d2629
Romain <r.chevallier21@hotmail.fr>
2018-12-14 11:47:50 +0100

    adding alfredIRC

---
+++ Alfred/alfredIRC/src/main/java/com/cesi/App.java
@@ -0,0 +1,18 @@
+package com.cesi;
+import java.nio.charset.StandardCharsets;
+import io.nats.jnats.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Connection nc = Nats.connect("nats://dev.lookingfora.name:4222");
+        nc.publish("irc.message.send", "{\"channel\":\"##cesi\",\"message\":\"Hello world\"}".getBytes(StandardCharsets.UTF_8));
+    }
+}
