966f3b2e30b148bea8d3a71b8b54fb28c95a1fd1
Romain <r.chevallier21@hotmail.fr>
2019-12-16 09:43:07 +0100

    [JavaScript] add covbadge.js

---
+++ Javascript/covbadge.js
@@ -0,0 +1,16 @@
+var lcov2badge = require('lcov2badge');
+lcov2badge.badge('../../proto_madera_front/coverage/lcov.info', function(err, svgBadge){
+    if (err) throw err;
+    console.log(svgBadge);
+});
+
+//var options = {
+//    filePath: './coverage/lcov.info',
+//    okColor: 'green', 					// default is 'brightgreen'
+//    warnColor: 'yellow', 				// default is 'orange'
+//    koColor: 'orange', 					// default is 'red'
+//    warnThreshold: 90, 					// default is 80
+//    koThreshold: 70,					// default is 60
+//    subject: 'cover'					// default is 'coverage'
+//};
+//lcov2badge.badge(options, function(err, svgBadge){...});
