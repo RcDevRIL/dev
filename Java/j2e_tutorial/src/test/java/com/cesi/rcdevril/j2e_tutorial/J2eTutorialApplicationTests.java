9f86871595395e7f28f35d4c48715b713c5323ff
Romain <r.chevallier21@hotmail.fr>
2020-01-31 09:07:30 +0100

    [J2E] Spring-boot tutorial
    - Initial commit

---
+++ Java/j2e_tutorial/src/test/java/com/cesi/rcdevril/j2e_tutorial/J2eTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesi.rcdevril.j2e_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
