d1e98088556a3838997b73f2ef471c6c7603b0ed
Romain <r.chevallier21@hotmail.fr>
2020-01-31 17:00:17 +0100

    [J2E] Spring-boot tutorial
    - Added two entities

---
+++ Java/j2e_tutorial/src/main/java/com/cesi/rcdevril/j2e_tutorial/entity/Category.java
@@ -0,0 +1,19 @@
+package com.cesi.rcdevril.j2e_tutorial.entity;
+
+public class Category {
+    private int categoryId;
+    private String category;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
---
+++ Java/j2e_tutorial/src/main/java/com/cesi/rcdevril/j2e_tutorial/entity/Website.java
@@ -0,0 +1,19 @@
+package com.cesi.rcdevril.j2e_tutorial.entity;
+
+public class Website {
+    private int websiteId;
+    private String url;
+
+    public int getWebsiteId() {
+        return websiteId;
+    }
+    public void setWebsiteId(int websiteId) {
+        this.websiteId = websiteId;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
